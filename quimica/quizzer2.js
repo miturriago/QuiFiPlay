e3fa9f0baf193294f44af534c3a7722df6b11496
miturriago <miturriago@uninorte.edu.co>
2019-11-21 21:46:20 -0500

    Add files via upload

---
+++ quimica/quizzer2.js
@@ -0,0 +1,208 @@
+
+var questions = [
+    [
+      "1.	La teoría atómica moderna explica las leyes de",
+      "Ley de la conservación de la masa, composición constante y proporciones múltiples.  ",
+      "Ley de proporciones múltiples, la temperatura y composición constante.",
+      "Ley de las cargas, la temperatura y conservación de la masa.",
+      "Ninguna de las anteriores",
+      0
+    ],
+    [
+      "2.	¿Cuantos postulados propuso Dalton?",
+      "Dos postulados",
+      "Un solo postulado",
+      "Tres postulados",
+      "Cuatro postulados",
+      2
+    ],
+    [
+        "3.	Los átomos se unen en una relación sencilla para formar:",
+       "Sustancias",
+       "Compuestos",
+       "Elementos",
+       "átomos",
+        1
+      ],
+      
+  ];
+  // 2
+  var questionTemplate = _.template(" \
+  <div class='card question'><span class='question'><%= question %></span> \
+  <ul class='options'> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='0' id='q<%= index %>o1'> \
+  <label for='q<%= index %>o1'><%= a %></label> \
+  </li> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='1' id='q<%= index %>o2'> \
+  <label for='q<%= index %>o2'><%= b %></label> \
+  </li> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='2' id='q<%= index %>o3'> \
+  <label for='q<%= index %>o3'><%= c %></label> \
+  </li> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='3' id='q<%= index %>o4'> \
+  <label for='q<%= index %>o4'><%= d %></label> \
+  </li> \
+  </ul> \
+  </div> \
+  ");
+  
+  // 3
+  var points,
+    pointsPerQuestion,
+    currentQuestion,
+    questionTimer,
+    timeForQuestion = 20, // seconds
+    timeLeftForQuestion;
+  // 4
+  $(function() {
+    // 5
+    $('button.start').click(start);
+    $('.play_again button').click(restart);
+  
+    // 6
+    function restart() {
+      points = 0;
+      pointsPerQuestion = 10;
+      currentQuestion = 0;
+      timeLeftForQuestion = timeForQuestion;
+      // 7
+      $('.finish.card').hide();
+      $('div.start').show();
+      $('.times_up').hide();
+  
+      generateCards();
+      updateTime();
+      updatePoints();
+    }
+  
+    // 8
+    function start() {
+      $('div.start').fadeOut(200, function() {
+        moveToNextQuestion();
+      });
+    }
+  
+    // 9
+    function generateCards() {
+      $('.questions').html('');
+      for (var i = 0; i < questions.length; i++) {
+        var q = questions[i];
+        var html = questionTemplate({
+          question: q[0],
+          index: i,
+          a: q[1],
+          b: q[2],
+          c: q[3],
+          d: q[4]
+        });
+        $('.questions').append(html);
+      };
+  
+      // 10
+      $('.question.card input').change(optionSelected);
+    }
+  
+    // 11
+    function moveToNextQuestion() {
+      currentQuestion += 1;
+      if (currentQuestion > 1) {
+        $('.question.card:nth-child(' + (currentQuestion - 1) + ')').hide();
+      }
+  
+      // 12
+      showQuestionCardAtIndex(currentQuestion);
+      setupQuestionTimer();
+    }
+  
+    // 13
+    function setupQuestionTimer() {
+      if (currentQuestion > 1) {
+        clearTimeout(questionTimer);
+      }
+      timeLeftForQuestion = timeForQuestion;
+      // 14
+      questionTimer = setTimeout(countdownTick, 1000);
+    }
+  
+    // 15
+    function showQuestionCardAtIndex(index) { // staring at 1
+      var $card = $('.question.card:nth-child(' + index + ')').show();
+    }
+  
+    // 16
+    function countdownTick() {
+      timeLeftForQuestion -= 1;
+      updateTime();
+      if (timeLeftForQuestion == 0) {
+        return finish();
+      }
+      questionTimer = setTimeout(countdownTick, 1000);
+    }
+  
+    // 17
+    function updateTime() {
+      $('.countdown .time_left').html(timeLeftForQuestion + 's');
+    }
+  
+    // 18
+    function updatePoints() {
+      $('.points span.points').html(points + ' puntos');
+    }
+  
+    // 19
+    function optionSelected() {
+      var selected = parseInt(this.value);
+      var correct = questions[currentQuestion - 1][5];
+  
+      if (selected == correct) {
+        points += pointsPerQuestion;
+        updatePoints();
+        correctAnimation();
+      } else {
+        wrongAnimation();
+      }
+  
+      if (currentQuestion == questions.length) {
+        clearTimeout(questionTimer);
+        return finish();
+      }
+      moveToNextQuestion();
+    }
+  
+    // 20
+    function correctAnimation() {
+      animatePoints('right');
+    }
+  
+    // 21
+    function wrongAnimation() {
+      animatePoints('wrong');
+    }
+  
+    // 22
+    function animatePoints(cls) {
+      $('header .points').addClass('animate ' + cls);
+      setTimeout(function() {
+        $('header .points').removeClass('animate ' + cls);
+      }, 500);
+    }
+  
+   
+  
+    // 23
+    function finish() {
+      if (timeLeftForQuestion == 0) {
+        $('.times_up').show();
+      }
+      $('p.final_points').html(points + ' puntos');
+      $('.question.card:visible').hide();
+      $('.finish.card').show();
+    }
+  
+    // 24
+    restart();
+  });
---
+++ quimica/quizzer4.js
@@ -0,0 +1,222 @@
+var questions = [
+    [
+      "1.  Bohr propone que:",
+      "Que los átomos tiene cargas neutras.",
+      "Que los átomos se desplazan alrededor del núcleo.",
+      "Que ni existe carga positiva en un  electrón.",
+      "Ninguna de las anteriores",
+      1
+    ],
+    [
+      "2. ¿Cuántos postulados tiene el modelo atómico de Bohr?",
+      "No tiene",
+      "Cuatro",
+      "Dos",
+      "Tres",
+      3
+    ],
+    [
+        "3. ¿los átomos se mueven alrededor del núcleo?",
+       "No",
+       "Si",
+       "Tal vez",
+       "Todas las anteriores",
+        1
+      ],
+     [
+        "4. cuándo los átomos se mueven en el núcleo ¿Qué pasa?",
+       "Gana y pierden electrones",
+       "Su carga es la misma",
+       "Su carga siempre quedan positivas",
+       "Su carga queda negativa",
+        0
+      ],
+      [
+        "5. ¿los átomos que se mueven alrededor del núcleo forman ondas?",
+       "No",
+       "Si",
+       "No es posible",
+       "Tal vez",
+        1
+      ],  
+  ];
+  // 2
+  var questionTemplate = _.template(" \
+  <div class='card question'><span class='question'><%= question %></span> \
+  <ul class='options'> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='0' id='q<%= index %>o1'> \
+  <label for='q<%= index %>o1'><%= a %></label> \
+  </li> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='1' id='q<%= index %>o2'> \
+  <label for='q<%= index %>o2'><%= b %></label> \
+  </li> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='2' id='q<%= index %>o3'> \
+  <label for='q<%= index %>o3'><%= c %></label> \
+  </li> \
+  <li> \
+  <input type='radio' name='question[<%= index %>]' value='3' id='q<%= index %>o4'> \
+  <label for='q<%= index %>o4'><%= d %></label> \
+  </li> \
+  </ul> \
+  </div> \
+  ");
+  
+  // 3
+  var points,
+    pointsPerQuestion,
+    currentQuestion,
+    questionTimer,
+    timeForQuestion = 20, // seconds
+    timeLeftForQuestion;
+  // 4
+  $(function() {
+    // 5
+    $('button.start').click(start);
+    $('.play_again button').click(restart);
+  
+    // 6
+    function restart() {
+      points = 0;
+      pointsPerQuestion = 10;
+      currentQuestion = 0;
+      timeLeftForQuestion = timeForQuestion;
+      // 7
+      $('.finish.card').hide();
+      $('div.start').show();
+      $('.times_up').hide();
+  
+      generateCards();
+      updateTime();
+      updatePoints();
+    }
+  
+    // 8
+    function start() {
+      $('div.start').fadeOut(200, function() {
+        moveToNextQuestion();
+      });
+    }
+  
+    // 9
+    function generateCards() {
+      $('.questions').html('');
+      for (var i = 0; i < questions.length; i++) {
+        var q = questions[i];
+        var html = questionTemplate({
+          question: q[0],
+          index: i,
+          a: q[1],
+          b: q[2],
+          c: q[3],
+          d: q[4]
+        });
+        $('.questions').append(html);
+      };
+  
+      // 10
+      $('.question.card input').change(optionSelected);
+    }
+  
+    // 11
+    function moveToNextQuestion() {
+      currentQuestion += 1;
+      if (currentQuestion > 1) {
+        $('.question.card:nth-child(' + (currentQuestion - 1) + ')').hide();
+      }
+  
+      // 12
+      showQuestionCardAtIndex(currentQuestion);
+      setupQuestionTimer();
+    }
+  
+    // 13
+    function setupQuestionTimer() {
+      if (currentQuestion > 1) {
+        clearTimeout(questionTimer);
+      }
+      timeLeftForQuestion = timeForQuestion;
+      // 14
+      questionTimer = setTimeout(countdownTick, 1000);
+    }
+  
+    // 15
+    function showQuestionCardAtIndex(index) { // staring at 1
+      var $card = $('.question.card:nth-child(' + index + ')').show();
+    }
+  
+    // 16
+    function countdownTick() {
+      timeLeftForQuestion -= 1;
+      updateTime();
+      if (timeLeftForQuestion == 0) {
+        return finish();
+      }
+      questionTimer = setTimeout(countdownTick, 1000);
+    }
+  
+    // 17
+    function updateTime() {
+      $('.countdown .time_left').html(timeLeftForQuestion + 's');
+    }
+  
+    // 18
+    function updatePoints() {
+      $('.points span.points').html(points + ' puntos');
+    }
+  
+    // 19
+    function optionSelected() {
+      var selected = parseInt(this.value);
+      var correct = questions[currentQuestion - 1][5];
+  
+      if (selected == correct) {
+        points += pointsPerQuestion;
+        updatePoints();
+        correctAnimation();
+      } else {
+        wrongAnimation();
+      }
+  
+      if (currentQuestion == questions.length) {
+        clearTimeout(questionTimer);
+        return finish();
+      }
+      moveToNextQuestion();
+    }
+  
+    // 20
+    function correctAnimation() {
+      animatePoints('right');
+    }
+  
+    // 21
+    function wrongAnimation() {
+      animatePoints('wrong');
+    }
+  
+    // 22
+    function animatePoints(cls) {
+      $('header .points').addClass('animate ' + cls);
+      setTimeout(function() {
+        $('header .points').removeClass('animate ' + cls);
+      }, 500);
+    }
+  
+   
+  
+    // 23
+    function finish() {
+      if (timeLeftForQuestion == 0) {
+        $('.times_up').show();
+      }
+      $('p.final_points').html(points + ' puntos');
+      $('.question.card:visible').hide();
+      $('.finish.card').show();
+    }
+  
+    // 24
+    restart();
+  });
